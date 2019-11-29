7427e94e1546d2a2f7e6d6ebff113e7873e3c902
Elliot Dawson <elliot.dawson@hp.com>
2019-11-28 21:21:05 -0700

    Flip image according to relative direction of destination hex

--- src/GamePiece.js
+++ src/GamePiece.js
@@ -5,11 +5,44 @@ class GamePiece {
         this.playerId = playerId;
         this.image = image;
         this.scale = scale;
+        this.facingDir = 'left';
     }
 
     moveTo(x,y) {
-        this.image.setX(x-50*this.scale);
-        this.image.setY(y-50*this.scale);
+
+        let currentX = this.image.getAttr('x');
+
+        if (x > currentX) {
+            if (this.facingDir === 'left') {
+                this.flipRight();
+                this.facingDir = 'right';
+            }
+            this.image.setX(x+50*this.scale);
+            this.image.setY(y-50*this.scale);
+        }
+        else if (x < currentX) {
+            if (this.facingDir === 'right') {
+                this.flipLeft();
+                this.facingDir = 'left';
+            }
+            this.image.setX(x-50*this.scale);
+            this.image.setY(y-50*this.scale);
+        }
+
+    }
+
+    flipRight() {
+
+        let x = this.image.getAttr('x');
+        let y = this.image.getAttr('y');
+    
+        this.image.setScale({x:-1});
+        this.image.rotateDeg(-15);
+    }
+    
+    flipLeft() {
+        this.image.setScale({x:1});
+        this.image.rotateDeg(15);
     }
 }
 
